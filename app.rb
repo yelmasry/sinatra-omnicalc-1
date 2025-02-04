e500d8f2686578c928a34c05440e709272ecfebc
yelmasry <yarra.elmasry@gmail.com>
2025-02-04 01:34:29 +0000

    stuck

--- app.rb
+++ app.rb
@@ -1,9 +1,78 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/howdy") do 
+  erb(:hello)
+end 
+
+get("/goodbye") do 
+  erb(:bye)
+end 
+
+
+###SQUARE 
+get("/square/new") do 
+  erb(:new_square_calc)
+end 
+
+get("/square/results") do 
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results) 
+end 
+
+
+
+### SQUARE ROOT
+get("/square_root/new") do 
+  erb(:new_squre_root_calc)
+end 
+
+get("/square_root/results") do 
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+
+  erb(:square_root_results) 
+end 
+
+
+
+
+###PAYMENT
+get("/payment/new") do 
+  erb(:payment)
 end
+
+get("/payment/results") do   
+  @apr = ((params.fetch("APR_user").to_f) / 100 / 12 ).round(4) #to_fs(:percentage, { :precision => 0 })
+  
+  @years = (params.fetch("years_user")).to_i * 12 
+
+  @principal = (params.fetch("principal_user")).to_f(:currency)
+
+  @numerator = @apr * @principal 
+
+  @denominator = 1 - (1 + @apr) ** @years 
+
+  @present_value = @numerator / @denominator 
+  
+  erb(:payment_results) 
+end 
+
+
+###RANDOM FORM
+get("/random/new") do 
+  erb(:random)
+end
+
+get("/random/results") do 
+  @min = params[:min].to_i
+  @max = params[:max].to_i 
+
+  @random_number = rand(@min..@max)
+
+  erb(:random_results)
+end 
